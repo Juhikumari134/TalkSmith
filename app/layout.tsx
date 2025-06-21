6d12becdb64b1bead8d1a9659ac2a387c96193d2
juhi134 <juhikushwaha9792@gmail.com>
2025-06-21 21:48:19 +0530

    Second Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const monaSans = Mona_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "PrepWise",
+  title: "TalkSmith",
   description: "An AI powered platform to prepare for mock interview",
 };
 
